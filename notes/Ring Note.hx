c0ac97b13090215c08477771b935f3e3045f50ab
team-504 <75187806+team-504@users.noreply.github.com>
2022-10-16 21:08:52 -0500

    rings funniness

--- notes/Ring Note.hx
+++ notes/Ring Note.hx
@@ -1,49 +1,54 @@
-    var rings:Int = 0;
-    var ringCounter:FlxSprite = null;
-    var counterNum:FlxText = null;
+enableRating = true;
 
-    function create() {
-        note.hitOnBotplay = true;
-        note.frames = Paths.getSparrowAtlas("note/rings");
-        note.colored = false;
-        switch(note.noteDirection) {
-            case 0:
-                note.animation.addByPrefix("scroll", "ring");
-            case 1:
-                note.animation.addByPrefix("scroll", "ring");
-            case 2:
-                note.animation.addByPrefix("scroll", "ring");
-            case 3:
-                note.animation.addByPrefix("scroll", "ring");
-            case 4:
-                note.animation.addByPrefix("scroll", "rring");
-        }
-        note.splashColor = 0xFFFFCC33;
-        
-        note.scale.set(0.7, 0.7);
-        note.updateHitbox();
-        note.maxEarlyDiff *= 0.5;
-        note.maxLateDiff *= 0.5;
+import flixel.text.FlxText;
+import flixel.text.FlxTextBorderStyle;
 
-        ringCounter = new FlxSprite(1133, 610).loadGraphic(Paths.image('events/ring count'));
-        add(ringCounter);
-        ringCounter.cameras = [PlayState.camHUD];
+var rings:Int = 0;
+var ccounter:FlxSprite = null;
 
-        counterNum = new FlxText(1207, 606, 0, 'null', 10, false);
-        counterNum.setFormat('Tw Cen MT Condensed Extra Bold', 60, FlxColor.fromRGB(255, 204, 51), FlxTextBorderStyle.OUTLINE, FlxColor.fromRGB(204, 102, 0));
-        counterNum.setBorderStyle(OUTLINE, FlxColor.fromRGB(204, 102, 0), 3, 1);
-        add(counterNum);
-        counterNum.cameras = [PlayState.camHUD];
+function create() {
+    note.hitOnBotplay = true;
+    note.frames = Paths.getSparrowAtlas("note/rings");
+    note.colored = false;
+    switch(note.noteDirection) {
+        case 0:
+            note.animation.addByPrefix("scroll", "ring instance 1");
+        case 1:
+            note.animation.addByPrefix("scroll", "ring instance 1");
+        case 2:
+            note.animation.addByPrefix("scroll", "ring instance 1");
+        case 3:
+            note.animation.addByPrefix("scroll", "ring instance 1");
+        case 4:
+            note.animation.addByPrefix("scroll", "ring instance 1");
     }
+    note.splashColor = 0xFFFFE600;
+    note.animation.addByPrefix("holdpiece", "hold piece");
+    note.animation.addByPrefix("holdend", "hold end");
+    
+    note.scale.set(0.7, 0.7);
+    note.updateHitbox();
+    note.maxEarlyDiff *= 0.5;
+    note.maxLateDiff *= 0.5;
+    note.cpuIgnore = false;
 
-    function onMiss() {
-    return false;
-    }
+    var counterpic = new FlxSprite(1133, 610).loadGraphic(Paths.image('events/ring count'));
+	add(counterpic);
+	counterpic.cameras = [PlayState.camHUD];
 
-    function onPlayerHit(direction:Int) {
+    counter = new FlxText(1207, 606, 0, '', 10, false);
+    counter.setFormat('counterfont', 60, 0xFFffcc33);
+    counter.setBorderStyle(FlxTextBorderStyle.OUTLINE, 0xFFcc6600, 3, 1);
+    counter.cameras = [PlayState.camHUD];
+    add(counter);
+}
+
+function onMiss() {
+    rings -= 1;
+}
+
+function onPlayerHit(direction:Int) {
     rings += 1;
-    }
-
-    function update(){
-    counterNum.text = rings;
-    }
+    trace(rings);
+    counter.text = rings;
+}
