1c85f9226a3a7c55b6ba63f755b58ff09583d579
team-504 <75187806+team-504@users.noreply.github.com>
2022-11-04 08:53:09 -0500

    lets start introing

---
+++ global_scripts/songthing.hx
@@ -0,0 +1,45 @@
+var startCircle:FlxSprite = null;
+var startText:FlxSprite = null;
+var blackness = new FlxSprite(-600, -400).makeGraphic(9999, 9999, 0xFF010101);
+
+function createPost() {
+    add(blackness);
+
+    startCircle = new FlxSprite(0, 0).loadGraphic(Paths.image('startassets/Circle-'+ PlayState.SONG.song.toLowerCase()));
+    startCircle.x += 900;
+    add(startCircle);
+    startText = new FlxSprite(0, 0).loadGraphic(Paths.image('startassets/Text-' + PlayState.SONG.song.toLowerCase()));
+    startText.x -= 1200;
+    add(startText);
+
+    new FlxTimer().start(0.6, function(tmr:FlxTimer)
+    {
+        FlxTween.tween(startCircle, {x: 0}, 0.5);
+        FlxTween.tween(startText, {x: 0}, 0.5);
+    });
+
+    new FlxTimer().start(1.9, function(tmr:FlxTimer)
+    {
+        FlxTween.tween(blackness, {alpha: 0}, 1, {
+            onComplete: function(twn:FlxTween)
+            {
+                remove(blackness);
+                blackness.destroy();
+            }
+        });
+        FlxTween.tween(startCircle, {alpha: 0}, 1, {
+            onComplete: function(twn:FlxTween)
+            {
+                remove(startCircle);
+                startCircle.destroy();
+            }
+        });
+        FlxTween.tween(startText, {alpha: 0}, 1, {
+            onComplete: function(twn:FlxTween)
+            {
+                remove(startText);
+                startText.destroy();
+            }
+        });
+    });
+}
